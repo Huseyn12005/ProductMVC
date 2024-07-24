247e121225936ea5b54d7a1010b1a18cc0eb25a3
Huseyn <horucov148@gmail.com>
2024-07-24 14:28:31 +0400

    Add project files.

---
+++ ProductMVC/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductMVC.Models;
+
+namespace ProductMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product>? products { get; set; }
+
+        static ProductController()
+        {
+            products = new List<Product>();
+            for (int i = 1; i <= 20; i++)
+            {
+                products.Add(new Product
+                {
+                    Id = i,
+                    Name = "Product " + i,
+                    Price = 10
+                });
+            }
+        }
+        public IActionResult GetAllProduct()
+        {
+            return View(products);
+        }
+
+        public IActionResult GetProductById(int id)
+        {
+            var product = products?.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        public IActionResult DeleteProductById(int id)
+        {
+            var product = products?.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                products?.Remove(product);
+            }
+            return RedirectToAction("GetAllProduct");
+        }
+    }
+}
